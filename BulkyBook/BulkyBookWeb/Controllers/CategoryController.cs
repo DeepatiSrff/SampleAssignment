b0ea8b04ad1f9a92fd4c35e06be7092b8e4835d0
Deepati Saraff <deepatisaraff@gmail.com>
2023-03-20 10:05:40 -0500

    First Commit

---
+++ BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index([Bind("WeightInKg,HeightInCm,BMI")] Category obj)
+        {
+            obj.BMI = Convert.ToString(((obj.WeightInKg) / (obj.HeightInCm * obj.HeightInCm)) * 10000);
+            
+            return View(obj);
+        }
+    }
+}
---
+++ BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Required]
+        [DisplayName("Weight (in KG)")]
+        public double WeightInKg { get; set; }
+        [Required]
+        [DisplayName("Height (in CM)")]
+        public double HeightInCm { get; set; }
+        public string BMI { get; set; } = string.Empty;
+    }
+}
